03d4e68b903a420a1c86675002d499bb8539ce96
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-02-22 09:59:08 +0200

    Update validation tests for Show

--- Tests/VGSShowSDKTests/VGSShowTests/Satellite Tests/VGSShowSatelliteUtilsTests.swift
+++ Tests/VGSShowSDKTests/VGSShowTests/Satellite Tests/VGSShowSatelliteUtilsTests.swift
@@ -1,17 +1,16 @@
 //
-//  VGSCollectSatelliteUtilsTests.swift
-//  VGSCollectSDK
+//  VGSShowSatelliteUtilsTests.swift
+//  FrameworkTests
 //
-//  Created on 16.02.2021.
+//  Created on 22.02.2021.
 //  Copyright © 2021 VGS. All rights reserved.
 //
 
 import XCTest
-//@testable import VGSCollectSDK
+@testable import VGSShowSDK
 
-/*
 /// Test satellite utils: local hostname and port validation, building satellite URL with valid/invalid configurations.
-class VGSCollectSatelliteUtilsTests: XCTestCase {
+class VGSShowSatelliteUtilsTests: VGSShowBaseTestCase {
 
 	/// Holds satellite test data.
 	struct SatelliteTestData {
@@ -21,19 +20,19 @@ class VGSCollectSatelliteUtilsTests: XCTestCase {
 	}
 
 	/// Set up satellite tests.
-	override class func setUp() {
+	override func setUp() {
 		super.setUp()
 
 		// Disable satellite assetions for unit tests.
-		VGSCollectSatelliteUtils.isAssertionsEnabled = false
+		VGSShowSatelliteUtils.isAssertionsEnabled = false
 	}
 
 	/// Tear down flow.
-	override class func tearDown() {
+	override func tearDown() {
 		super.tearDown()
 
 		// Enable satellite assertions.
-		VGSCollectSatelliteUtils.isAssertionsEnabled = true
+		VGSShowSatelliteUtils.isAssertionsEnabled = true
 	}
 
 	/// Test satellite hostname normalization with IP address.
@@ -46,7 +45,7 @@ class VGSCollectSatelliteUtilsTests: XCTestCase {
 			if let normalized = ipHostnames[index].normalizedHostname() {
 				XCTAssert(normalized == validNormalizedIPHostname)
 			} else {
-				assertionFailure("cannot normalize: \(ipHostnames[index])")
+				XCTFail("cannot normalize: \(ipHostnames[index])")
 			}
 		}
 	}
@@ -71,11 +70,11 @@ class VGSCollectSatelliteUtilsTests: XCTestCase {
 		let invalidPorts = [-1, 0, 100000000, 1000000000]
 
 		for index in 0..<validPorts.count {
-			XCTAssertTrue(VGSCollectSatelliteUtils.isSatellitePortValid(validPorts[index]), "*\(validPorts[index]) is valid port.*")
+			XCTAssertTrue(VGSShowSatelliteUtils.isSatellitePortValid(validPorts[index]), "*\(validPorts[index]) is valid port.*")
 		}
 
 		for index in 0..<invalidPorts.count {
-			XCTAssertFalse(VGSCollectSatelliteUtils.isSatellitePortValid(invalidPorts[index]), "*\(invalidPorts[index]) is invalid port.*")
+			XCTAssertFalse(VGSShowSatelliteUtils.isSatellitePortValid(invalidPorts[index]), "*\(invalidPorts[index]) is invalid port.*")
 		}
 	}
 
@@ -84,7 +83,7 @@ class VGSCollectSatelliteUtilsTests: XCTestCase {
 		let validhostnames = ["localhost", "http://localhost", "http:localhost", "192.168.1", "192.168.1.3", "192.168.1.5", "http://192.168.1.3"]
 
 		for index in 0..<validhostnames.count {
-			XCTAssertTrue(VGSCollectSatelliteUtils.isSatelliteHostnameValid(validhostnames[index]), "*\(validhostnames[index]) is valid hostname.*")
+			XCTAssertTrue(VGSShowSatelliteUtils.isSatelliteHostnameValid(validhostnames[index]), "*\(validhostnames[index]) is valid hostname.*")
 		}
 	}
 
@@ -93,7 +92,7 @@ class VGSCollectSatelliteUtilsTests: XCTestCase {
 		let invalidhostnames = ["google", "http://google.com", "http://localhostsomebackend", "193.168.1", "192.169.1.3", "192.170.1.5", "http://190.168.1.3"]
 
 		for index in 0..<invalidhostnames.count {
-			XCTAssertFalse(VGSCollectSatelliteUtils.isSatelliteHostnameValid(invalidhostnames[index]), "*\(invalidhostnames[index]) is invalid hostname.*")
+			XCTAssertFalse(VGSShowSatelliteUtils.isSatelliteHostnameValid(invalidhostnames[index]), "*\(invalidhostnames[index]) is invalid hostname.*")
 		}
 	}
 
@@ -129,7 +128,7 @@ class VGSCollectSatelliteUtilsTests: XCTestCase {
 		for index in 0..<testData.count {
 			let config = testData[index]
 			let outputText = "index: \(index) satellite configuration with hostname: \(config.hostname) port: \(config.port) should produce: \(config.satelliteURL!)"
-			if let url = VGSCollectSatelliteUtils.buildSatelliteURL(with: "sandbox", hostname: config.hostname, satellitePort: config.port) {
+			if let url = VGSShowSatelliteUtils.buildSatelliteURL(with: "sandbox", hostname: config.hostname, satellitePort: config.port) {
 				XCTAssertTrue(url == config.satelliteURL!, "error: \(url) != \(config.satelliteURL!) - " + outputText)
 			} else {
 				XCTFail(outputText)
@@ -186,7 +185,7 @@ class VGSCollectSatelliteUtilsTests: XCTestCase {
 
 		for index in 0..<testData.count {
 			let config = testData[index]
-			let invalidURL = VGSCollectSatelliteUtils.buildSatelliteURL(with: "sandbox", hostname: config.hostname, satellitePort: config.port)
+			let invalidURL = VGSShowSatelliteUtils.buildSatelliteURL(with: "sandbox", hostname: config.hostname, satellitePort: config.port)
 			let outputText = "index: \(index) satellite invalid configuration with hostname: \(config.hostname) port: \(config.port) should produce *nil*, actuallResult: \(invalidURL?.absoluteString ?? "*nil*")"
 			XCTAssertTrue(invalidURL == nil, outputText)
 		}
@@ -206,7 +205,7 @@ class VGSCollectSatelliteUtilsTests: XCTestCase {
 		for index in 0..<testData.count {
 			let environment = testData[index]
 			let outputText = "index: \(index) satellite invalid environment: \(environment) but valid hostname: \(configuration.hostname) port: \(configuration.port) should produce *nil*"
-			let invalidURL = VGSCollectSatelliteUtils.buildSatelliteURL(with: environment, hostname: configuration.hostname, satellitePort: configuration.port)
+			let invalidURL = VGSShowSatelliteUtils.buildSatelliteURL(with: environment, hostname: configuration.hostname, satellitePort: configuration.port)
 			XCTAssertTrue(invalidURL == nil, outputText)
 		}
 	}
@@ -224,7 +223,7 @@ class VGSCollectSatelliteUtilsTests: XCTestCase {
 		for index in 0..<validTestIPs.count {
 			let ip = validTestIPs[index]
 			let outputText = "index: \(index) satellite ip: \(ip) should be valid"
-			XCTAssertTrue(VGSCollectSatelliteUtils.verifyIPHostNameIsCorrect(ip), outputText)
+			XCTAssertTrue(VGSShowSatelliteUtils.verifyIPHostNameIsCorrect(ip), outputText)
 		}
 
 		let invalidTestIPs = [
@@ -237,9 +236,7 @@ class VGSCollectSatelliteUtilsTests: XCTestCase {
 		for index in 0..<invalidTestIPs.count {
 			let ip = invalidTestIPs[index]
 			let outputText = "index: \(index) satellite ip: \(ip) should be invalid"
-			XCTAssertFalse(VGSCollectSatelliteUtils.verifyIPHostNameIsCorrect(ip), outputText)
+			XCTAssertFalse(VGSShowSatelliteUtils.verifyIPHostNameIsCorrect(ip), outputText)
 		}
 	}
 }
-
-*/
