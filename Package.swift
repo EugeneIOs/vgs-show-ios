81032ab3988c9ad5000dc33c59218bd46032307e
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-02-22 18:07:56 +0200

    Update package

--- Package.swift
+++ Package.swift
@@ -19,7 +19,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VGSShowSDK",
-						resources: [.process("Resources")]),
+					  exclude: ["Info.plist"],
+						resources: [.process("Resources"),
+												.process("Info.plist")]),
 				.testTarget(
             name: "VGSShowSDKTests",
             dependencies: ["VGSShowSDK"])
